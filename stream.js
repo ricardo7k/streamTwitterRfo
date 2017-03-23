4c7a8e0aaea629f2b98e795abe40451ebe2ab799
Caca Ferreira <ricardo7k@yahoo.com.br>
2017-03-23 15:09:25 -0300

    Text Ajust

--- stream.js
+++ stream.js
@@ -1,13 +1,13 @@
 var Twitter = require('twitter');
 
 var client = new Twitter({
-  consumer_key: "",
-  consumer_secret: "",
-  access_token_key: "",
-  access_token_secret: ""
+  consumer_key: "coGuTMm23MMEvNXblHWViN1L5",
+  consumer_secret: "JQkfDuS5a6B3fv12Ub30pACrdzYBtNRVygVoOGl3UP9B3VIYuo",
+  access_token_key: "3145-zUEttXKXkgwRWoYVQGrjtryBaZcrcflKxvl5nrdNFaMYm",
+  access_token_secret: "Tq0Akg5QqGIvna8Z0o0ypLveoOtIbnry42JGDBJ3ZIfAw"
 });
 
-client.stream('statuses/filter', {track: 'ricardo7k'},  function(stream) {
+client.stream('statuses/filter', {track: 'com%20ricardo7k'},  function(stream) {
   stream.on('data', function(tweet) {
     console.log(tweet.text);
   });
