813dd0b12cd55297f937963254f1e960eee155d5
Caca Ferreira <ricardo7k@yahoo.com.br>
2017-07-18 12:43:30 -0300

    page

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta charset='UTF-8'>
+<meta http-equiv="Cache-control" content="public">
+<meta name="coinzilla" content="coinzilla-verification"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+<meta name="language" content="en"/>
+<meta name="description" content=""/>
+<meta name="keywords" content=""/>
+<title>Stream react</title>
+<link rel="stylesheet" href="/assets/css/reset.css" type="text/css" charset="utf-8"/>
+<link rel="stylesheet" href="/assets/css/fade.css" type="text/css" charset="utf-8"/>
+<link rel="stylesheet" href="/assets/css/main.css" type="text/css" charset="utf-8"/>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" type="text/css" charset="utf-8"/>
+</head>
+<body>
+<section id="content" class="opaco">
+<div id="navigate-async">
+<header>
+<h2 id="sub-title">
+1 - 2
+</h2>
+</header>
+</body>
+</html>
